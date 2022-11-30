e20d9b42109cbbaca5380a56de9e8cdbb949dc4d
acle <ayzirekakyshova04@gmail.com>
2022-11-30 10:30:54 +0600

    first commit

---
+++ main/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Post
+
+class PostSerializer(ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+    def to_representation(self, instance):
+        #print("instance:",instance)
+        rep = super().to_representation(instance)    
+       # print("repr", rep)
+        rep["author"] = instance.author.username
+        return rep 
+

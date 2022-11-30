3989ad8bdbd900ac1af0a2b9ff7db7305e00eb91
acle <ayzirekakyshova04@gmail.com>
2022-11-30 11:55:48 +0600

    admin, search, filter

--- main/models.py
+++ main/models.py
@@ -7,3 +7,6 @@ class Post(models.Model):
     author =models.ForeignKey(User, related_name='Posts', on_delete=models.CASCADE)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True) 
+
+    def __str__(self):
+        return f'{self.author} -> {self.body}' 
